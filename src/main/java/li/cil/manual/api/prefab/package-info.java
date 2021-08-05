ed59c12d387a92624c9f87e89f3c970886060d64
Florian Nücke <florian@nuecke.de>
2021-08-05 18:11:28 +0200

    Fix package annotation.

--- src/main/java/li/cil/manual/api/prefab/package-info.java
+++ src/main/java/li/cil/manual/api/prefab/package-info.java
@@ -1,4 +1,7 @@
 @ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
 package li.cil.manual.api.prefab;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
 
 import javax.annotation.ParametersAreNonnullByDefault;
