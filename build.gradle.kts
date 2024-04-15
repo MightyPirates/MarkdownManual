ab6cbb150f339820c3bc6c7516d3eed3d6d03fd5
Florian Nücke <florian@nuecke.de>
2024-04-16 00:36:06 +0200

    Optimize imports.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import net.fabricmc.loom.api.LoomGradleExtensionAPI
 import net.fabricmc.loom.task.RemapJarTask
-import org.gradle.configurationcache.extensions.capitalized
 
 plugins {
     java
